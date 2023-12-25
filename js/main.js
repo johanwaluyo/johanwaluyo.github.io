cfa2295ed604320e2ea0a4751e1dfb2bb3b7d757
johan waluyo <johan_waluyo@yahoo.com>
2023-12-25 19:25:43 +0800

    add button play song
    add button play song

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
-    });
+    });   
 
 
     // Gallery carousel
